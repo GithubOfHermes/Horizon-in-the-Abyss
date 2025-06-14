db1fba4801c4a88f57acd7ed6634fa18dda25218
GithubOfHermes <hermes21916@gmail.com>
2025-06-14 15:58:10 +0900

    UI_Version 1.0.0

---
+++ _UI/Coin_UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin_UI : MonoBehaviour
+{
+    public PlayerStat playerStat;
+    public Text Coin_Text;
+
+    void Start()
+    {
+        if (playerStat == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+                playerStat = player.GetComponent<PlayerStat>();
+        }
+    }
+
+    void Update()
+    {
+        if (playerStat != null && Coin_Text != null)
+        {
+            Coin_Text.text = playerStat.currentCoin.ToString();
+        }
+    }
+}
+
